a679e7f10327a40f570fec936479e0fa8a318cb9
Ronny <ronnybet41@gmail.com>
2022-08-27 15:56:28 +0300

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-const init = () => {
-  
-}
 
-document.addEventListener('DOMContentLoaded', init);
+
+const init = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let input = event.target.querySelector("input").value;
+      console.log(input);
+      fetch("http://localhost:3000/movies")
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          // LOG: (3) [{…}, {…}, {…}]
+        });
+    });
+  };
+  
+  document.addEventListener("DOMContentLoaded", init);
